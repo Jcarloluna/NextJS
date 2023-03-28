1707422ab0a881c822b9a50b3ae1498c0f189bfc
Jcarloluna <91709785+Jcarloluna@users.noreply.github.com>
2023-03-28 12:41:32 +0800

    icon title

--- next-js-jamm/src/pages/index.js
+++ next-js-jamm/src/pages/index.js
@@ -2,11 +2,18 @@ import MainNavigation from "@/components/MainNavigation";
 import Hero from "@/components/Hero";
 import { Fragment } from "react";
 import Footer from "@/components/Footer";
+import Head from "next/head";
 
 
 const HomePage = () => {
   return (
     <Fragment>
+      <Head>
+        <link rel="shortcut icon" href="/images/appIcon.png" />
+        <title>
+          JAMM
+        </title>
+      </Head>
       <MainNavigation/>
       <Hero/>
       <Footer/>
